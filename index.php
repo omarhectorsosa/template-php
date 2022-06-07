82f96f928171d2dc922a75916ff71620e55c482b
Omar Sosa <ososa@psa.com.ar>
2022-06-07 13:08:22 -0300

    Cambia conexion

--- index.php
+++ index.php
@@ -24,8 +24,8 @@
      * */    
 
     //if (!($conexion=mysqli_connect("us-cdbr-east-03.cleardb.com","b019e5d7c7a033","615a27bc","heroku_1ae4bba01a595f3"))) { 
-    if (!($conexion=mysqli_connect("sql5.freemysqlhosting.net","sql5496827","tAee5z4mwN","sql5496827"))) { 
-    //if (!($conexion=mysqli_connect("127.0.0.1","root","password","perfumeria"))) { 
+    //if (!($conexion=mysqli_connect("sql5.freemysqlhosting.net","sql5496827","tAee5z4mwN","sql5496827"))) { 
+    if (!($conexion=mysqli_connect("2.tcp.ngrok.io:15720","root","password","almacen"))) { 
         echo '<div class="row"><div class="col-12">Error conectando a la base de datos</div></div>' ;
         echo '</main></body>';  
         die(); 
