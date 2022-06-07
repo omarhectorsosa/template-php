29302d21f39ef89f6ca13f40135e31324981ede8
Omar Sosa <ososa@psa.com.ar>
2022-06-07 20:35:34 -0300

    Corrge host

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cambiocd</title>
+    <title>Cambio en clase</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
     <style>
       @media (min-width: 992px) {
@@ -20,7 +20,7 @@
     echo '<div class="row text-center bg-success mb-10"><div class="col-12">Tabla.</div></div>' ;
 
     // Conecto a una base de datos local (user hgrok) 
-    if (!($conexion=mysqli_connect("2.tcp.ngrok.io:15720","root","password","almacen"))) { 
+    if (!($conexion=mysqli_connect("8.tcp.ngrok.io:15079 ","root","password","almacen"))) { 
         echo '<div class="row"><div class="col-12">Error conectando a la base de datos</div></div>' ;
         echo '</main></body>';  
         die(); 
